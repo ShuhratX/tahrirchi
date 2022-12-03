bad7f53c7d2e66d6e11615f9fb603272cef82a20
ShuhratXayitov <65951559+ShuhratX@users.noreply.github.com>
2022-12-04 00:23:58 +0500

    debug

--- word_in_dataset.py
+++ word_in_dataset.py
@@ -10,7 +10,7 @@ fl = open('test_dataset.csv', 'w', encoding='UTF8')
 
 header = ['source_url', 'access_datetime', 'content', 'word']
 writer = csv.writer(fl, delimiter='&')
-writer.writerow(header)
+# writer.writerow(header)
 
 urls = ['https://kun.uz/uz/news/2022/12/01/uch-tomonlama-ittifoq-tashqi-kuchlar-qolidagi-ozbek-gazi-va-volodinning-hurmatsizligi-siyosatshunoslar-bilan-suhbat', 'https://stadion.uz/uz/news/detail/359943', 'https://qalampir.uz/uz/news/2050-yilga-borib-5-milliarddan-kup-odam-suv-tank-isligiga-uchraydi-bmt-73059', 'https://daryo.uz/2022/12/02/ozbekistonda-dam-olish-kunlari-qanday-ob-havo-boladi-3/']
 for url in urls:
@@ -23,17 +23,6 @@ for url in urls:
 		content.append(pg.get_text())
 	words = ' '.join(content)
 	word = words.split(" ")
-
 	data = [url, acc_time, content, word]
 	writer.writerow(data)
 
-# d_set = pd.read_csv('test_dataset.csv', sep='&')
-# # print(d_set)
-# for wr in d_set['content']:
-# 	# print(wr)
-# 	res = wr.split(" ")
-# 	print(res)
-# 	# d_set.loc[f'{wr}', 'word'] = res
-# 	# d_set['word'] = d_set['word'].replace({'NaN': res})
-# 	# d_set.to_csv("test_dataset.csv", index=False)
-# 	# print(d_set['word'])
